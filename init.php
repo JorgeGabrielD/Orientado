b3e0b2ea6b3afa38194a8cccbfadc2616a4076b0
Jorge Delgado <jgdddelgado@hotmail.com>
2022-06-07 21:06:10 -0300

    First commit

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','id18842313_satorin');
+define('BD_SENHA','<NpDeQChpyTJjoI7}');
+define('BD_BANCO','id18842313_db_livraria');
+?>
